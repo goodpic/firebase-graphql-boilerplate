e743441127003a91b23bd26a014c6199b985fb2c
goodpic <kaneko@gmail.com>
2019-06-20 17:47:20 +0100

    Fix spec

--- functions/src/firebase/__tests__/myfirestore.spec.ts
+++ functions/src/firebase/__tests__/myfirestore.spec.ts
@@ -3,19 +3,19 @@ const serviceAccount = require("../../../config/YOUR-PROJECT-NAME-firebase-admin
 
 describe('MyFirestore', () =>{
   it('getDocument', async () => {
-    
+
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount),
       databaseURL: "https://unittest-dfb64.firebaseio.com"
     });
-    let result = false
     const db = admin.firestore();
     const collectionRef = db.collection('test')
-    const docRef = collectionRef.doc('testDoc')
+    const docRef = collectionRef.doc('testdata')
     const getDoc = await docRef.get()
     if (getDoc && getDoc.exists) {
-      result = getDoc.data()
+      const { testParam } = getDoc.data()
+      expect(testParam).toEqual('testValue')
     }
-    expect(result).toEqual('testdata')
+
   }, 10000)
 })
